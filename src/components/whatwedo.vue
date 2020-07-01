f0a8f45011ed415a9eb73927840e6f6340796c09
Edith <edith.chevallier@hotmail.fr>
2020-07-01 14:04:06 +0200

    added overlay

--- src/components/whatwedo.vue
+++ src/components/whatwedo.vue
@@ -1,138 +1,208 @@
 <template>
-    <div class="card whatwedo">
-        <div class="card-body">
-            <h1 class="card-title">What we do</h1>
-            <p class="card-text">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-            </p>
-        </div>
+    <div class="whatwedo-container">
+        <div class="card whatwedo">
+            <div class="card-body">
+                <h1 class="card-title">What we do</h1>
+                <p class="card-text">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
 
-        <div class="container">
-            <div class="row">
-                <progressive-img
-                    id="pic"
-                    src="/pasta-min.jpg"
-                    placeholder="/pasta.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="Responsive image"
-                />
-                <progressive-img
-                    id="pic"
-                    src="/gardengreen-min.jpg"
-                    placeholder="/gardenGreen.jpg"
-                    class="rounded float-left col-sm img-fluid"
-                    alt="Responsive image"
-                />
-                <progressive-img
-                    id="pic"
-                    src="/housePaint-min.jpg"
-                    placeholder="/housePaint.jpg"
-                    class="rounded float-left col-sm img-fluid"
-                    alt="Responsive image"
-                />
-            </div>
-            <div class="row">
-                <progressive-img
-                    id="pic"
-                    src="/flowers-min.jpg"
-                    placeholder="/flowers.jpg"
-                    class="rounded float-left col-sm img-fluid"
-                    alt="..."
-                />
-
-                <progressive-img
-                    id="pic"
-                    src="/sun-min.jpg"
-                    placeholder="/sun.jpg"
-                    class="rounded float-left col-sm img-fluid"
-                    alt="..."
-                />
-                <progressive-img
-                    id="pic"
-                    src="/green-min.jpg"
-                    placeholder="/green.jpg"
-                    class="rounded float-left col-sm img-fluid"
-                    alt="..."
-                />
-            </div>
-            <div class="row">
-                <progressive-img
-                    id="pic"
-                    src="/12633-min.jpg"
-                    placeholder="/12633.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-                <progressive-img
-                    id="pic"
-                    src="/pink-min.jpg"
-                    placeholder="/pink.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-                <progressive-img
-                    id="pic"
-                    src="/couture-min.jpg"
-                    placeholder="/couture.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-            </div>
-            <div class="row">
-                <progressive-img
-                    id="pic"
-                    src="/fuego-min.jpg"
-                    placeholder="/fuego.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-                <progressive-img
-                    id="pic"
-                    src="/sits-min.jpg"
-                    placeholder="/sits.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-                <progressive-img
-                    id="pic"
-                    src="/cooking.png"
-                    placeholder="/cooking.png"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-            </div>
-            <div class="row">
-                <progressive-img
-                    id="pic"
-                    src="/red-min.jpg"
-                    placeholder="/red.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-
-                <progressive-img
-                    id="pic"
-                    src="/painting-min.png"
-                    placeholder="/painting.png"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
-                <progressive-img
-                    id="pic"
-                    src="/leaves-min.jpg"
-                    placeholder="/leaves.jpg"
-                    class="rounded float-left col-sm img-fluid "
-                    alt="..."
-                />
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-md-4 " style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/pasta-min.jpg"
+                            placeholder="/pasta.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/green-min.jpg"
+                            placeholder="/gardenGreen.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/housePaint-min.jpg"
+                            placeholder="/housePaint.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/flowers-min.jpg"
+                            placeholder="/flowers.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/sun-min.jpg"
+                            placeholder="/sun.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/green-min.jpg"
+                            placeholder="/green.jpg"
+                            class="rcard-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <!-- <div class="row"> -->
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/12633-min.jpg"
+                            placeholder="/12633.jpg"
+                            class="card-img-top "
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/pink-min.jpg"
+                            placeholder="/pink.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/couture-min.jpg"
+                            placeholder="/couture.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <!-- <div class="row"> -->
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/fuego-min.jpg"
+                            placeholder="/fuego.jpg"
+                            class="card-img-top "
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/sits-min.jpg"
+                            placeholder="/sits.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/cooking.png"
+                            placeholder="/cooking.png"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <!-- <div class="row"> -->
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/red-min.jpg"
+                            placeholder="/red.jpg"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/painting-min.png"
+                            placeholder="/painting.png"
+                            class="card-img-top"
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            id="pic"
+                            src="/leaves-min.jpg"
+                            placeholder="/leaves.jpg"
+                            class="card-img-top "
+                            alt="Responsive image"
+                        />
+                        <div class="middle">
+                            <div class="text">Pastatafel - Aout 2019</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -170,26 +240,66 @@ export default {
 .whatwedo {
     height: auto;
     width: 90%;
-    margin: 200px auto;
+    margin: 100px auto;
     padding-bottom: 50px;
 }
 
-/* .progressive-image-wrapper {
-    width: 33% !important;
-} */
-
-.rounded.float-left.progressive-image {
-    min-width: 33% !important;
+#pic {
     height: 200px !important;
-    object-fit: cover !important;
+
+    opacity: 1;
+    display: block;
+    width: 100%;
+    height: auto;
+    transition: 0.5s ease;
+    backface-visibility: hidden;
+}
+img.progressive-image-main {
+    height: 200px !important;
+}
+.card-img-top {
+    /* min-width: 33% !important; */
+    /* height: 200px !important; */
+    /* width: 100% \9; */
+    /* object-fit: cover !important; */
     margin: 0.15%;
     transition: transform 0.2s;
+    margin-bottom: 10px;
 }
 
-.rounded.float-left.progressive-image:hover {
+/* .rounded.float-left.progressive-image:hover {
     transform: scale(1.2);
     z-index: 100;
     border: white solid 4px;
     object-fit: cover;
+} */
+
+.whatwedo-container {
+    padding-top: 50px;
+}
+
+.middle {
+    transition: 0.5s ease;
+    opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+.col-6.col-md-4:hover #pic {
+    opacity: 0.3;
+}
+
+.col-6.col-md-4:hover .middle {
+    opacity: 1;
+}
+
+.text {
+    color: black;
+    font-size: 16px;
+    padding: 16px 32px;
 }
 </style>
