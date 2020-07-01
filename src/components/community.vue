032d3a1386f5491b69e2cf242e6ccaac15cfd767
Edith <edith.chevallier@hotmail.fr>
2020-07-01 12:45:19 +0200

    What we do

--- src/components/community.vue
+++ src/components/community.vue
@@ -23,21 +23,134 @@
             <div class="card-body">
                 <h1 class="card-title">Community</h1>
                 <p class="card-text">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum.
+                    VGG garden is composed of a wide variety of people and bees
+                    where everyone is welcome. We are gardeners, builders, party
+                    bees with different background, culture, colors, gender,
+                    talents.
                 </p>
             </div>
 
-            <div class="container">
+            <!-- <div class="container">
                 <div class="row"></div>
+            </div> -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            class="card-img-top"
+                            id="protrait"
+                            src="/green-min.jpg"
+                            placeholder="/pasta.jpg"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            class="card-img-top"
+                            id="protrait"
+                            src="/green-min.jpg"
+                            placeholder="/pasta.jpg"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            class="card-img-top"
+                            id="protrait"
+                            src="/green-min.jpg"
+                            placeholder="/pasta.jpg"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            class="card-img-top"
+                            id="protrait"
+                            src="/green-min.jpg"
+                            placeholder="/pasta.jpg"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- <div class="row"> -->
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            class="card-img-top"
+                            id="protrait"
+                            src="/green-min.jpg"
+                            placeholder="/pasta.jpg"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            class="card-img-top"
+                            id="protrait"
+                            src="/green-min.jpg"
+                            placeholder="/pasta.jpg"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4" style="width: 18rem;">
+                        <progressive-img
+                            class="card-img-top"
+                            id="protrait"
+                            src="/green-min.jpg"
+                            placeholder="/pasta.jpg"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- </div> -->
             </div>
         </div>
     </div>
