f20527dddef349feeac75185a9ded0f756528803
Edith <edith.chevallier@hotmail.fr>
2020-07-23 17:51:24 +0200

    website fully set up for translation plus need to adjust navbar

--- src/components/tandc.vue
+++ src/components/tandc.vue
@@ -11,7 +11,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">
-                        Terms and Conditions
+                        {{ $t("TermsAndConditions")}}
                     </h5>
                     <button
                         type="button"
@@ -24,34 +24,17 @@
                 </div>
                 <div class="modal-body">
                     <p>
-                        Like most similar websites, this site uses cookies. On
-                        this page we explain more about cookies and how we use
-                        them. By using this website and agreeing to this policy,
-                        you consent to our use of cookies in accordance with the
-                        terms of this policy.
+                    {{ $t("tandcDesc1")}}
+
                     </p>
-                    <h2>About cookies</h2>
+                    <h2>{{ $t("AboutCookies")}}</h2>
                     <p>
-                        Cookies are files, often including unique identifiers,
-                        that are sent by web servers to web browsers, and which
-                        may then be sent back to the server each time the
-                        browser requests a page from the server. Cookies can be
-                        used by web servers to identify and track users as they
-                        navigate different pages on a website, and to identify
-                        users returning to a website. Cookies may be either
-                        “persistent” cookies or “session” cookies. A persistent
-                        cookie consists of a text file sent by a web server to a
-                        web browser, which will be stored by the browser and
-                        will remain valid until its set expiry date (unless
-                        deleted by the user before the expiry date). A session
-                        cookie, on the other hand, will expire at the end of the
-                        user session, when the web browser is closed.
+                       {{ $t("tandcDesc2")}}
                     </p>
 
-                    <h2>Cookies on this website</h2>
+                    <h2>{{ $t("CookiesWebsite")}}</h2>
                     <p>
-                        We use both session cookies and persistent cookies on
-                        this website.
+                       {{ $t("tandcDesc3")}}
                     </p>
                 </div>
                 <div class="modal-footer">
@@ -60,7 +43,7 @@
                         class="btn btn-secondary"
                         data-dismiss="modal"
                     >
-                        Close
+                        {{ $t("Close")}}
                     </button>
                 </div>
             </div>
--- src/plugin/i18n.js
+++ src/plugin/i18n.js
@@ -62,12 +62,13 @@ const messages = {
     Thank: "Thank you",
     ThankMessage: "for reaching out to us. We will come back to you as soon as possible!",
     Close: "Close",
-    
-    
-    
-    
-    
     tandC: "TandC",
+    TermsAndConditions: " Terms and Conditions",
+    tandcDesc1: " Like most similar websites, this site uses cookies. On this page we explain more about cookies and how we use them.By using this website and agreeing to this policy, you consent to our use of cookies in accordance with the terms of this policy.",
+    AboutCookies: "About cookies",
+    tandcDesc2: "Cookies are files, often including unique identifiers, that are sent by web servers to web browsers, and which may then be sent back to the server each time the browser requests a page from the server.Cookies can be used by web servers to identify and track users as they navigate different pages on a website, and to identify users returning to a website.Cookies may be either persistent cookies or session cookies.A persistent cookie consists of a text file sent by a web server to a web browser, which will be stored by the browser and will remain valid until its set expiry date(unless deleted by the user before the expiry date).A session cookie, on the other hand, will expire at the end of the user session, when the web browser is closed.",
+    CookiesWebsite: "Cookies on this website",
+    tandcDesc3: "    We use both session cookies and persistent cookies on this website.",
     }, 
     'de': {
         About: "Uber",
